7756849a14a0cd05bf2e6cef7abcfdc489bbf15c
Waqar1982 <waquar82@gmail.com>
2025-03-20 10:37:01 +0100

    a

--- Fra skjerm til modell/model.js
+++ Fra skjerm til modell/model.js
@@ -3,67 +3,71 @@ const model = {
 app:{
     page: [
         'startPage',
-        'allActivites',
-        'searchActivities',
-        'addActivities',
-        'showActivities',
+        'dashbord',
+        'koffeinkalkulator',
+        'logg',
+        'timer',
         ],
 
-        popUpSettings: false,
-        filterPopUp: false,
 
 },
 
 inputs:{
-    addActivity: {
+    editItem:{
+        name:'',
+        price: null,
+        koffein: null,
+        id: null
+
+    },
+
+    oppdaterLogg: {
+        dato: null,
+        koffein
+    },
+
+    loggInn: {
+        userName:'',
+        password: '',
+    },
+
+createUser:{
+    name: '',
         
-            id: null,
-            title: '',
-            dificulty: null,
-            timeInMinutes: null,
-            category: [],
-            equipment: [],
-            how: '',
-            img: '',
- }
-
-
+    
+},  
 
 },
+
+    
+    
 
 data: {
-    category: [],
-
-    activityList:[
-        {
-            id: null,
-            title: '',
-            dificulty: '',
-            timeInMinutes: '',
-            category: [],
-            equipment: [],
-            how: '',
-            img: '',
-
-        },
-        {
-            id: null,
-            title: '',
-            dificulty: '',
-            timeInMinutes: '',
-            category: [],
-            equipment: [],
-            how: '',
-            img: '',
-
-        },
-
-
+    koffeinProdukter: [
+        { id: 1, name: 'Red Bull', koffeinInnholdDl: 32, koffeinInnholdEnhet: 80, itemGroup: 1, pris: 30},
+        { id: 2, name: 'Monster', koffeinInnholdDl: 32, koffeinInnholdEnhet: 80, itemGroup: 1, pris: 30},
+        { id: 3, name: 'Burn', koffeinInnholdDl: 32, koffeinInnholdEnhet: 80, itemGroup: 1, pris: 30},
+        { id: 10, name: 'Kaffe 1', koffeinInnholdDl: 45, koffeinInnholdEnhet: 90, itemGroup: 2, pris: 30},
+        { id: 11, name: 'Kaffe 2', koffeinInnholdDl: 40, koffeinInnholdEnhet: 80, itemGroup: 2, pris: 30},
+        { id: 20, name: 'Coca Cola', koffeinInnholdDl: 10, koffeinInnholdEnhet: 50, itemGroup: 3, pris: 30},
     ],
+    produktGrupper: [
+        {id: 1, gruppeNavn: 'EnergiDrikker', beskrivelse: '???'},
+        {id: 2, gruppeNavn: 'Kaffe', beskrivelse: '???'},
+        {id: 3, gruppeNavn: 'Annet', beskrivelse: '???'}
+    ],
+        
+    koffeinLogg: [],
 
-
-},
-
+    motivasjonsBoost: ['Eksempel: Et kaldt glass vann er bra!'],
+        
+    category: [],
+        
+        
+        
+        
+    },
+    
 
 }
 
